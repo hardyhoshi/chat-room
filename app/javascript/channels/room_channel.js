d3cd4f412370c386390a77ad8ab9d258338149f5
hardyhoshi <hardyhoshi@gmail.com>
2021-02-28 16:36:47 +0900

    redisの接続確認

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,7 +1,8 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
+consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2 }, {
   connected() {
+    console.log("connected...")
     // Called when the subscription is ready for use on the server
   },
 
